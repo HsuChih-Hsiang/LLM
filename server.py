eecca4b8e36d5044fcf183b0f746380173bb3d4f
Chih-Hsiang <chihhsianghsu0410@gmail.com>
2024-09-10 14:53:00 +0800

    change setup llm method & add favicon

--- server.py
+++ server.py
@@ -1,17 +1,34 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
+from contextlib import asynccontextmanager
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
+from fastapi.responses import Response
 from llm_model import LLM_MODEL
 from chat_room import Room
 import uvicorn
 import uuid
 
 
-llm = LLM_MODEL()     
-app = FastAPI()
+
 templates = Jinja2Templates(directory="templates")
 room_dict = {}
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global llm
+    llm = LLM_MODEL()
+    yield
+   
+app = FastAPI(lifespan=lifespan)
+
+
+templates = Jinja2Templates(directory="templates")
+room_dict = {}
+llm = None
+
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return Response(content=b"", media_type="image/x-icon")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
@@ -42,7 +59,15 @@ async def websocket_endpoint(websocket:WebSocket, room_id: str):
             room.connections.remove(websocket)
             if len(room.connections) == 0:
                 del room_dict[room_id]
+ 
+@app.post("/documents")               
+async def add_documents():
+    pass
+
+@app.get("/documents")               
+async def add_documents():
+    pass
 
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", reload=True)
+    uvicorn.run("server:app", host="140.112.3.52", port=8000, reload=True)
