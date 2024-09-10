2c5e1fe8be9f533650653dc0967c7a0d7e1a28e0
Chih-Hsiang <chihhsianghsu0410@gmail.com>
2024-09-10 10:09:26 +0800

    refactor

--- DB/text_rag.py
+++ DB/text_rag.py
@@ -1,28 +1,43 @@
-from typing import Dict, Type, List, Any
-from dependency_injector import containers, providers
-from psycopg2.extensions import connection
+from typing import Dict, Type, List, Callable, Any
+import psycopg2.extras
+from psycopg2.extensions import connection, cursor
 from psycopg2.pool import SimpleConnectionPool
 from sentence_transformers import SentenceTransformer
 import PyPDF2
 
-class DB_UTILITY:
+class DataBaseUtility:
     def __init__(self, db_connection):
         self.db = db_connection
         
-    def db_conn_template(self, func):
+    def db_conn_template(self, func: Callable) -> Callable:
         def wrapper(*args, **kwargs):
             conn = self.db.getconn()
             try:
                 return func(self, conn, *args, **kwargs)
             finally:
                 self.db.putconn(conn)
-        return wrapper  
+        return wrapper
+    
+    def db_trans_dict(self, func: Callable) -> Callable:
+        def wrapper(self, conn: connection, *args, **kwargs):
+            with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+                func(self, cursor, *args, **kwargs)
+                result = cursor.fetchall()
+                return [dict(row) for row in result] if result else []
+        return wrapper
+    
+    def db_commit(self, func: Callable) -> Callable:
+        def wrapper(self, conn: connection, *args, **kwargs):
+            with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+                func(self, cursor, *args, **kwargs)
+                conn.commit()
+        return wrapper
 
-class DB_CONN:
-    _instance: Type["DB_CONN"] = None
+class DataBaseConnection:
+    _instance: Type["DataBaseConnection"] = None
     pool: SimpleConnectionPool = None
 
-    def __new__(cls, db_arg: Dict[str, str], minconn: int = 1, maxconn: int = 10) -> Type["DB_CONN"]:
+    def __new__(cls, db_arg: Dict[str, str], minconn: int = 1, maxconn: int = 10) -> Type["DataBaseConnection"]:
         if cls._instance is None:
             cls._instance = super().__new__(cls)
             cls.pool = SimpleConnectionPool(
@@ -40,80 +55,53 @@ class DB_CONN:
     def putconn(cls, conn: connection) -> None:
         cls.pool.putconn(conn)
 
-class DB_CREATE(DB_UTILITY):
-    def __init__(self, db_connection: DB_CONN):
+class DataBaseCreate(DataBaseUtility):
+    def __init__(self, db_connection: DataBaseConnection):
         super().__init__(db_connection)
-        if self.extend_check() and self.table_check():
-            self.add_extension()
+        self.add_extension()
+        if not self.table_check(): 
             self.create_table()
+            
+    @DataBaseUtility.db_trans_dict
+    @DataBaseUtility.db_conn_template
+    def table_list(self, cur: cursor) -> None:
+        cur.execute("SELECT * FROM information_schema.tables")
+        
+    @DataBaseUtility.db_commit
+    @DataBaseUtility.db_conn_template
+    def create_table(self, cur: cursor, table_command: str) -> None:
+        cur.execute(table_command)
+        
+    @DataBaseUtility.db_commit
+    @DataBaseUtility.db_conn_template
+    def add_extension(self, cur: cursor) -> None:
+        cur.execute("CREATE EXTENSION IF NOT EXISTS pgvector")
 
-    @DB_UTILITY.db_conn_template
-    def extend_check(self, conn: connection) -> Dict:
-        with conn.cursor() as cur:
-            cur.execute("SELECT * FROM pg_extension")
-            extend_exist = cur.fetchall()
-        return extend_exist
-
-    @DB_UTILITY.db_conn_template
-    def table_check(self, conn: connection) -> Dict:
-        with conn.cursor() as cur:
-            cur.execute("SELECT * FROM information_schema.tables")
-            table_exist = cur.fetchall()
-        return table_exist
-
-    @DB_UTILITY.db_conn_template
-    def create_table(self, conn: connection) -> None:
-        with conn.cursor() as cur:
-            cur.execute("""
-                CREATE TABLE IF NOT EXISTS your_table_name (
-                    id SERIAL PRIMARY KEY,
-                    column1 TEXT,
-                    column2 INTEGER
-                )
-            """)
-            conn.commit()
-
-    @DB_UTILITY.db_conn_template
-    def add_extension(self, conn: connection) -> None:
-        with conn.cursor() as cur:
-            cur.execute("CREATE EXTENSION IF NOT EXISTS pgvector")
-            conn.commit()
     
-class RAG(DB_UTILITY):
-    def __init__(self, db_connection: DB_CONN) -> None:
+class RAG(DataBaseUtility):
+    def __init__(self, db_connection: DataBaseConnection) -> None:
         super().__init__(db_connection)
+        self.model = SentenceTransformer('all-MiniLM-L6-v2')
     
     @staticmethod
-    def encoding_text(text: str) -> List[float]:
-        model = SentenceTransformer('all-MiniLM-L6-v2')
-        embedding = model.encode(text).tolist()
-        return embedding
+    def encoding_text(self, text: str) -> List[float]:
+        return self.model.encode(text).tolist()
 
-    @classmethod
-    @DB_UTILITY.db_conn_template
-    def store_pdf(cls, conn: connection, pdf: bytes) -> None:
+    @DataBaseUtility.db_commit
+    @DataBaseUtility.db_conn_template
+    def store_pdf(cls, cur: cursor, pdf: bytes) -> None:       
         with open(pdf, 'rb') as pdf_file:
             reader = PyPDF2.PdfReader(pdf_file)
-            text = ""
-            for page_num in range(len(reader.pages)):
-                page = reader.pages[page_num]
-                text += page.extract_text()
+            text = "".join(page.extract_text() for page in reader.pages)
 
         embedding = cls.encoding_text(text)
-        
-        with conn.cursor() as cur:
-            cur.execute("INSERT INTO documents (file_name, embedding) VALUES (%s, %s)", (text, embedding))
-            cls.conn.commit()
-       
+        cur.execute("INSERT INTO documents (file_name, embedding) VALUES (%s, %s)", (text, embedding))
 
-    @classmethod
-    @DB_UTILITY.db_conn_template
-    def retrieve_pdfs(cls, conn: connection, query: str) -> List[str]:
+    @DataBaseUtility.db_trans_dict
+    @DataBaseUtility.db_conn_template
+    def retrieve_pdfs(cls, cur: cursor, query: str, limit: int = 5) -> List[Dict[str, Any]]:
         query_embedding = cls.encoding_text(query)
-        
-        with conn.cursor() as cur:
-            cur.execute("SELECT pdf_path FROM documents ORDER BY embedding <-> %s LIMIT 5", (query_embedding))
-            results = cur.fetchall()
-
+        cur.execute("SELECT pdf_path FROM documents ORDER BY embedding <-> %s LIMIT %s", (query_embedding, limit))
+        results = cur.fetchall()
         return [result[0] for result in results]
     
