2fafad963d36bb46cee1616d0c6a64c3d7fd2b1c
Chih-Hsiang <chihhsianghsu0410@gmail.com>
2024-09-24 11:36:44 +0800

    file structure modify & LLM model indepent

--- Model/llm_model.py
+++ Model/llm_model.py
@@ -1,33 +1,27 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer, QuantoConfig
+from Model.model_enum import MODEL_INFO
 from threading import Thread, Lock
-from enum import Enum
 import torch
 
-class MODEL_INFO(Enum):
-    BREEZE = {
-        "model_name": "MediaTek-Research/Breeze-7B-Instruct-v1_0",
-        "torch_dtype": torch.bfloat16
-    }
-    
-    TAIWAN_LLM = {
-        "model_name": "MediaTek-Research/Breeze-7B-Instruct-v1_0",
-        "torch_dtype": torch.bfloat16,
-        "quanto_config": QuantoConfig(weights="int4")
-    }
-    
-    @property
-    def model_name(self):
-        return self.value.get("model_name")
+class LLMModel:
+    _instance = None 
+    _lock = Lock()
 
-    @property
-    def torch_dtype(self):
-        return self.value.get("torch_dtype")
-    
-    @property
-    def quanto_config(self):
-        return self.value.get("quanto_config")
+    @classmethod
+    def generater_response(self, message):
+        chat = [
+        {"role": "user", "content": message},
+        ]
+        conversion = self.tokenizer.apply_chat_template(chat, tokenize=False, add_generation_prompt=False)
+        encoding = self.tokenizer(conversion, return_tensors="pt").to(self.device)
+        streamer = TextIteratorStreamer(self.tokenizer)
+        generation_kwargs = dict(encoding, streamer=streamer, max_new_tokens=1000)
+        thread = Thread(target=self.model.generate, kwargs=generation_kwargs)
+        thread.start()
 
-class BreezeModel:
+        return conversion, streamer
+
+class BreezeModel(LLMModel):
     _instance = None 
     _lock = Lock()
 
@@ -44,20 +38,6 @@ class BreezeModel:
                 cls.device = torch.device("cuda")
                 cls.tokenizer = AutoTokenizer.from_pretrained(model_name)
             return cls._instance
-        
-    @classmethod
-    def generater_response(self, message):
-        chat = [
-        {"role": "user", "content": message},
-        ]
-        conversion = self.tokenizer.apply_chat_template(chat, tokenize=False, add_generation_prompt=False)
-        encoding = self.tokenizer(conversion, return_tensors="pt").to(self.device)
-        streamer = TextIteratorStreamer(self.tokenizer)
-        generation_kwargs = dict(encoding, streamer=streamer, max_new_tokens=1000)
-        thread = Thread(target=self.model.generate, kwargs=generation_kwargs)
-        thread.start()
-
-        return conversion, streamer
     
 class TaiwanLLMModel(BreezeModel):
     _instance = None 
