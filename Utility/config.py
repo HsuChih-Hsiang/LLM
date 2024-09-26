fdef55270ab6060d5a31d398c93e519d896de89c
Chih-Hsiang <chihhsianghsu0410@gmail.com>
2024-09-26 10:45:59 +0800

    add config class

---
+++ Utility/config.py
@@ -0,0 +1,33 @@
+import os
+import yaml
+import json
+import toml
+import logging
+from enum import Enum
+
+class ConfigKey(Enum):
+    CACHE_DIR = "cache_dir"
+
+class Configuration:
+    def __init__(self, config: str = 'init_config.yml'):
+        self.config: dict = {}
+        try:
+            if os.path.isfile(config):
+                with open(config, "r") as stream:
+                    if config.endswith(".yaml") or config.endswith(".yml"):
+                        self.config = yaml.safe_load(stream)
+                    elif config.endswith(".json"):
+                        self.config = json.load(stream)
+                    elif config.endswith(".toml"):
+                        self.config = toml.load(stream)
+                    else:
+                        raise ValueError(f"Unsupported file format: {config}")
+        except FileNotFoundError:
+            logging.warning(f"Configuration file '{config}' not found. Using default settings.")
+        except (yaml.YAMLError, json.JSONDecodeError, toml.TomlDecodeError) as e:
+            logging.error(f"Error parsing {config}: {e}")
+
+    def get_value(self, key: str):
+        return self.config.get(key)
+
+ 
